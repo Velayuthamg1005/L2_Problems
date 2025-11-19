a72e4bf1ec765f578a5b6d5ce03eae74cec176e9
Velayuthamg1005 <velayuthamg1005@gmail.com>
2025-11-19 19:34:19 +0530

    Solved LC27 | 19/11/25 19.34

---
+++ src/Array/LC27_RemoveElement.java
@@ -0,0 +1,26 @@
+package Array;
+
+public class LC27_RemoveElement {
+
+    public static void main(String[] args) {
+
+        int[] nums={0,1,2,2,3,0,4,2};
+        int val=2;
+
+        System.out.println("Result: "+removeElement(nums,val));
+    }
+
+    public static int removeElement(int[] nums,int val){
+
+        int j=0;
+        int n=nums.length;
+
+        for(int i=0;i<n;i++){
+            if(nums[i]!=val){
+                nums[j++]=nums[i];
+            }
+        }
+
+        return j;
+    }
+}
